bd857525b670e8f66924683f16e61e65dabb02ab
Zach <zach.kroeze@gmail.com>
2015-01-20 13:22:41 -0500

    deleting directory

--- Knapsack - Jan 18th 2014/move_down_dir.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-if($_SESSION['logged_in']==1){
-	$_SESSION['dir']=$_SESSION['dir'] . $_GET["folder"];
-	header("Location: http://ownbox.no-ip.biz/user.php");
-} else{
-	echo "Not logged in";
-}
-?>
--- Knapsack - Jan 18th 2014/unlink.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-if($_SESSION['logged_in']==1){
-		rmdir("/var/www/Documents/TEST1");
-		echo "It works";
-}else{
-	echo "Not logged in";
-}
-?>
